57eaefd1d1e769e7f463cae3b7fa048076921d08
trouiller luka <lukatrouiller@gmail.com>
2023-03-29 13:29:25 +0200

    Chapitre4 symfony, récupérer les données d'un formulaire (ajout d'un template email)

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+// use Symfony\Component\Mime\Email; (la ligne est inutile)
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 class BaseController extends AbstractController
 {
@@ -18,10 +23,31 @@ class BaseController extends AbstractController
 
 
     #[Route('/contact', name: 'contact')]
-    public function contact(): Response
-    {
+    public function contact(Request $request, MailerInterface $mailer): Response    {
+        $form = $this->createForm(ContactType::class);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $email = (new TemplatedEmail())
+                ->from($form->get('email')->getData())
+                ->to('reply@nuage-pedagogique.fr')
+                ->subject($form->get('sujet')->getData())
+                ->htmlTemplate('emails/email.html.twig')
+                ->context([
+                    'nom'=> $form->get('nom')->getData(),
+                    'sujet'=> $form->get('sujet')->getData(),
+                    'message'=> $form->get('message')->getData()
+                ]);
+              
+                $mailer->send($email);
+                $this->addFlash('notice','Message envoyé');
+                return $this->redirectToRoute('contact');
+            }
+        }
+
         return $this->render('base/contact.html.twig', [
-            
+            'form' => $form->createView()
         ]);
     }
 
@@ -37,7 +63,7 @@ class BaseController extends AbstractController
     public function metionlegales(): Response
     {
         return $this->render('base/mention-legales.html.twig', [
-            
+
         ]);
     }
 }
